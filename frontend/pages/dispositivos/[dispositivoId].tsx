0fd7cdf44ccaebf86495cdc58865b7e080dc7602
Cristian Garcia <cmg-88@hotmail.com>
2024-06-09 23:15:21 -0300

    primer commit

---
+++ frontend/pages/dispositivos/[dispositivoId].tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useDispositivoById,useDispositivoList,useLogsList,useLogsById} from "./../../api"
+import { Item,Layout,ItemLog,ItemLogs } from "./../../Components"
+import { Paper, Typography, Button } from "@mui/material"
+
+
+const DispositivoId: NextPage = () => {
+    const router = useRouter();
+    const { dispositivoId } = router?.query;
+    
+    const onBack = () => {
+        router.back();
+    }
+
+    if(dispositivoId !== undefined){
+       console.log(dispositivoId);
+       
+    }
+
+    const { dispositivo } =  useDispositivoById(dispositivoId);
+    //console.log("arreglo de dispositivo dispositivoId");
+    console.log(dispositivo);
+    const { logs } = useLogsById(dispositivo?.dispositivoId);
+    // console.log("arreglo de logs dispositivoId");
+    // console.log(dispositivoId);
+    //console.log(logs);
+
+    
+
+    return (
+      <Layout>
+        <div style={{ backgroundColor: 'white',textAlign: 'center'  }}>
+          <h1>LOGS</h1>
+        </div>
+         <Button   variant="outlined"  onClick={onBack} > Volver    </Button>
+          {
+            logs?.map((element: ItemLogs, index: number) => <ItemLog key={index}
+                logId={element.logId}
+                ts={element.ts}
+                etemperatura={element.etemperatura}
+                nodoId={element.nodoId}
+            />)
+            
+          }
+          <Button   variant="outlined"  onClick={onBack} > Volver    </Button>
+          
+        </Layout>
+    ) 
+}
+
+export default DispositivoId;
